011d98661bbaf93e3435841743840ea2d3974ab3
Daniel Barkley <dbbarkley93@gmail.com>
2019-05-05 08:27:43 -0500

    added pictures and frames.

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Item.css";
 
 const Item = props => (
-  <div className="col-6 col-lg-3 ">
+  <div className="col-6 col-lg-3">
   <div
     onClick={() => props.handleClick(props.id)}
     style={{ backgroundImage: `url("${props.image}")` }}
